c84491caa439090b265deaafe3dd0e8ee94f97de
dino1997milan <96890515+dino1997milan@users.noreply.github.com>
2022-11-01 13:03:08 +0100

    Add files via upload

---
+++ app/build/generated/source/buildConfig/debug/com/getmotivation/getmotivation/OrlandoAntonioVeneziano/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.getmotivation.getmotivation.OrlandoAntonioVeneziano;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.getmotivation.getmotivation.OrlandoAntonioVeneziano";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
